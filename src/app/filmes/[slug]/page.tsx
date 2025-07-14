08de8145ae1df6b42177ac076b237296ea149581
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-07-14 01:05:36 -0300

    fixed redlines

--- src/app/filmes/[slug]/page.tsx
+++ src/app/filmes/[slug]/page.tsx
@@ -1,12 +1,6 @@
 // src/app/filmes/[slug]/page.tsx
 import FilmeClientPage from './FilmeClientPage';
 
-type PageProps = {
-    params: {
-        slug: string;
-    };
-};
-
 export default function FilmePageWrapper({ params }: any) {
     return <FilmeClientPage slug={params.slug} />;
 }
