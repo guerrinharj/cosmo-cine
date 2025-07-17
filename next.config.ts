a7cac2958b2f63e5b4d62939a5a454dd78a56d35
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-07-17 06:09:38 -0300

    commented i18n on next config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['pt', 'en'],
-    defaultLocale: 'pt',
-    localeDetection: false
-  },
+// i18n: {
+//   locales: ['pt', 'en'],
+//   defaultLocale: 'pt',
+//   localeDetection: false
+// }
   images: {
     domains: ['i.vimeocdn.com', 'vumbnail.com', 'vimeo.com']
   }
