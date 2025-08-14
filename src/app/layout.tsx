83500ff4c94edceffb0be43cfb998f5402707e9e
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-08-14 07:16:47 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-'use client'; // required at the top of layout if using hooks
+'use client';
 
 import { useEffect } from 'react';
 import { Geist, Geist_Mono } from "next/font/google";
@@ -6,40 +6,23 @@ import "./globals.css";
 import NavBar from '../components/NavBar';
 import { usePathname } from 'next/navigation';
 
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     document.title = '| COSMO CINE DO BRASIL | Produção Audiovisual';
   }, []);
 
-
   const pathname = usePathname();
-
   const hideNavBar = pathname.startsWith('/filmes/') && pathname !== '/filmes';
 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logos/COM ICONE/Cosmo_V_positivo_Icone.png" />
+        <link rel="icon" href="/logos/COM%20ICONE/Cosmo_V_positivo_Icone.png" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {!hideNavBar && <NavBar />}
         {children}
       </body>
