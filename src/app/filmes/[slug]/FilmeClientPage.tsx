7ce85d8f07995a010daf6757231bb3b85e4c124f
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-08-30 16:23:51 -0300

    path to filmes

--- src/app/filmes/[slug]/FilmeClientPage.tsx
+++ src/app/filmes/[slug]/FilmeClientPage.tsx
@@ -102,7 +102,7 @@ export default function FilmeClientPage({ slug }: { slug: string }) {
                     &larr;
                 </button>
 
-                <button onClick={() => router.push('/')} className="text-sm">
+                <button onClick={() => router.push('/filmes')} className="text-sm">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="h-6 w-6 transition-transform duration-500 hover:rotate-[360deg]"
